17b047c6848d4096d2e12fd852c05be84c37f22d
Kat Schmidt <siliconfoxx@protonmail.com>
2024-06-17 19:51:59 -0400

    dll changes

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbe360ec60fdb77fa72a6f9a28da21283326d297")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb572557db2b1aa4e7dcb4605aa4abb1b62b3943")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
